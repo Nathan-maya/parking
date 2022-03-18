096665eea5f03af9b3825121f2ee36f18881b885
nathan.maia <61885401+Nathan-maya@users.noreply.github.com>
2022-03-17 21:20:41 -0300

    novas correcoes no slide

--- js/modules/carrossel.js
+++ js/modules/carrossel.js
@@ -2,39 +2,27 @@ export default function carrossel() {
   let time = 2000,
     currentImageIndex = 0,
     images = document.querySelectorAll('.container-slider img'),
-    radio = document.querySelectorAll('.container-radio input'),
-    maxImage = images.length,
-    maxRadio = radio.length;
+    radio = document.querySelectorAll('.container-label input'),
+    maxImage = images.length;
 
-    const timer = setInterval(() => {
+  console.log(radio)
+  function start(){ 
+    setInterval(() => {
       nextImage()
-    },time);
+      },time);
+    }
 
-    console.log(radio)
   function nextImage() {
     images[currentImageIndex].classList.remove("selected")
-    radio[currentImageIndex].removeAttribute('checked')
+    radio[currentImageIndex].checked = false;
     
     currentImageIndex++
     if (currentImageIndex >= maxImage){
       currentImageIndex = 0
     }
     images[currentImageIndex].classList.add("selected")
-    radio[currentImageIndex].setAttribute('checked','checked')
+    radio[currentImageIndex].checked = true;
   }
 
-  function radioChecked(){
-    radio[currentImageIndex].removeAttribute('checked')
-    radio.forEach((item , index) => {
-      images[index].classList.remove("selected");
-      if (radio[index].checked) {
-        images[index].classList.add("selected");
-      }
-    })
-  }
-
-  for (let i = 0; i < maxRadio; i++){
-    radio[i].addEventListener("click",radioChecked)
-  }
   window.addEventListener("load",start)
 }
